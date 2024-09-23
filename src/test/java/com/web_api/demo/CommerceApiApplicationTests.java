a9b6d893836e286432d94b3366509c0333a6c2b8
Alex_Pasini <alexantoiopasini20@gmail.com>
2024-09-23 14:23:25 -0300

    initial commit

---
+++ src/test/java/com/web_api/demo/CommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
